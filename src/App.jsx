43e8918e246a08f13313f5e230f08a81f2c72d94
JitishxD <jitish08@gmail.com>
2026-01-27 17:28:13 +0530

    feat: ScrollToTop whenever the `pathname` changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 
 import NavBar from "./Components/NavBar/NavBar";
 import Tech from "./pages/Tech";
@@ -8,6 +8,16 @@ import WebDTeam from "./pages/WebDTeam";
 import AllEvents from "./pages/AllEventsPage";
 import SplashScreen from "./Components/SplashScreen/SplashScreen";
 import Footer from "./Components/Footer/Footer";
+
+function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    }, [pathname]);
+
+    return null;
+}
 
 function App() {
     const [showSplash, setShowSplash] = useState(true);
@@ -18,6 +28,7 @@ function App() {
 
     return (
         <>
+            <ScrollToTop />
             <NavBar />
             <main>
                 <Routes>
